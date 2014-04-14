f17e69a182daca161f735be094a9f6ec3425944f
Chris Laskey <contact@chrislaskey.com>
2014-04-14 14:32:27 -0400

    Updated Add Tree form to make address optional

--- app/forms/addtreeform.py
+++ app/forms/addtreeform.py
@@ -1,12 +1,11 @@
 from flask_wtf import Form
-from wtforms import SelectField, SubmitField, TextField
-from wtforms.validators import AnyOf, DataRequired, Optional
-from . customfields import TagItField
+from wtforms import SubmitField, TextField
+from wtforms.validators import DataRequired, Optional
 
 
 class AddTreeForm(Form):
     longitude = TextField('Longitude', validators=[DataRequired()])
     latitude = TextField('Latitude', validators=[DataRequired()])
-    address = TextField('Address')
+    address = TextField('Address', validators=[Optional()])
     common_name = TextField('Common Name', validators=[DataRequired()])
     submit = SubmitField('Add New Tree')
