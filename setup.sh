adbbabe2a27c7ba60d4b83d9edd763906a9a6468
Chris Laskey <contact@chrislaskey.com>
2014-04-27 22:48:40 -0400

    Updated setup environment script

--- setup.sh
+++ setup.sh
@@ -4,7 +4,7 @@
 # installing setting up a virtualenv and importing requirements.txt into pip.
 # Can be run multiple times, it is written to be idempotent.
 #
-# Version 1.5.0
+# Version 1.5.2
 
 this_file=`basename "$0"`
 this_path=`cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd`
@@ -16,19 +16,7 @@ required_minimum_python_version_minor=''
 required_minimum_python_version_minor_minor=''
 virtualenvs_dir="/usr/lib/virtualenvs"
 project_virtualenv_path="${virtualenvs_dir}/${project_name}" 
-environment_json_file="./app/environment.json"
 individual_setup_files_dir="./setup"
-
-# option_force=
-# parse_options () {
-#	while getopts "f" opt; do
-#		case $opt in
-#			f)
-#				option_force="true"
-#				;;
-#		esac
-#	done
-# } ; parse_options $@
 
 set -o nounset
 set -o errtrace
@@ -128,42 +116,6 @@ install_pip_packages_into_virtualenv_if_requires_file () {
 	fi
 }
 
-create_json_file_with_environment_information () {
-	# TODO: Deprecated
-	# Original use for environment.json file no longer needed.
-	if [[ -f "${environment_json_file}" ]]; then
-		if ! rm "${environment_json_file}"; then
-			error "Could not remove environment JSON file, '${environment_json_file}'."
-		fi
-	fi
-
-	if ! touch "${environment_json_file}"; then
-		error "Could not create environment JSON file, 'touch ${environment_json_file}'."
-	fi
-
-	# Use the virtualenv python binary to export the virtualevn site-packages
-	# directory, which is not a standard location as it includes the specific
-	# python version in the path, e.g. ".venv/lib/python2.7/site-packages/".
-	site_packages_path=`${project_virtualenv_path}/bin/python -c "from distutils.sysconfig import get_python_lib; print(get_python_lib())"`
-	site_packages_dir=`echo ${site_packages_path} | sed -e "s#$(pwd)/##"`
-
-	# virtualenv path may be relative, this makes sure the path is absolute.
-	original_pwd=`pwd`
-	project_virtualenv_abspath=`cd ${project_virtualenv_path}; pwd`
-	cd "$original_pwd"
-	
-	env_data="{"
-	env_data="${env_data} \"base_path\": \"`pwd`\", "
-	env_data="${env_data} \"virtualenv_dir\": \"${project_virtualenv_path}\", "
-	env_data="${env_data} \"virtualenv_path\": \"${project_virtualenv_abspath}\", "
-	env_data="${env_data} \"site_packages_dir\": \"${site_packages_dir}\", "
-	env_data="${env_data} \"site_packages_path\": \"${site_packages_path}\" "
-	env_data="${env_data} }"
-
-	# Use python -mjson to pretty print JSON into file
-	echo ${env_data} | python -mjson.tool > ${environment_json_file}
-}
-
 execute_individual_setup_files () {
 	if [[ ! -d "${individual_setup_files_dir}" ]]; then
 		return 0
@@ -173,6 +125,8 @@ execute_individual_setup_files () {
 		source "${filename}"
 	done
 }
+
+# Application execution
 
 verify_python_version
 verify_virtualenv_binary_installed
